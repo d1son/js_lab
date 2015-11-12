67912f6b984a54f401598e9ab876eb976011965f
Dong Son <d1son@nbp-214-197.nbp.ruw.rutgers.edu>
2015-11-11 21:30:26 -0500

    added basic html and js for the in class lab

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+var question;
+
+var question = prompt("type something here")
+	if (typeof question === ) {
+		console.log ("this is a string")
+	} else {
+		console.log ("this is not a string")
+	}
+
+
