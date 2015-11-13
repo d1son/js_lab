1b1580e2df7bdd8ac8b42009cbc5c841fd1e6b5f
Dong Son <d1son@Dongs-MacBook-Pro.local>
2015-11-12 21:18:07 -0500

    added variable to determine if the input was a string or number, and to determine which number was greater

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,21 @@
-var question;
+var firstValue = prompt("Input a number or a word");
 
-var question = prompt("type something here")
-	if (typeof question === ) {
-		console.log ("this is a string")
-	} else {
-		console.log ("this is not a string")
-	}
+firstValue = parseInt(firstValue);
 
+if(isNaN(firstValue)) {
+	console.log ("This is a String!")
+} else {
+	console.log ("This is a Number!")
+}
 
+var number1 = prompt("Input a number");
+var number2 = prompt("Input a number");
+
+number1 = (parseInt(number1));
+number2 = (parseInt(number2));
+
+if(number1 > number2) {
+	console.log (number1 + " is grater!")
+} else {
+	console.log (number2 + " is greater!")
+}
